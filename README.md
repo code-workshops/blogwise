467e3255aab4ed623f4a95435e8647969eab4cfd
Kay Hudson <kayhudson@DESKTOP-V3RDJT1.localdomain>
2018-11-08 10:06:02 -0800

    Add documentation.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blog Wise
+
+A blogging application. Formatted as a demonstration of the following:
+
+* MVP branch
+* Sprint 1 branch: Planning documents, project skeleton (working server)
+* Sprint 2 branch: MVP
+* Sprint 3 branch: Final
+* Master branch: Full featured app
--- views.py
+++ views.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect
 
-from models import db, Article
-
 app = Flask(__name__)
 
+
+# Step 1: Get 1 route working for sanity check. See server.py for more step 1
 
 @app.route('/')
 def index():
@@ -14,6 +14,7 @@ def index():
     * View function (this function)
     """
     
-    # Try to replace your print() statements with logs instead anywhere that you're testing the live server.
+    # Try to replace your print() statements with logs instead anywhere 
+    # that you're testing the live server.
     app.logger.info("Home page loaded.")
     return render_template('index.html')
