fc6441fa096e76881a454fb3501edd24159f4c5c
Protosac <protosacq@gmail.com>
2018-12-30 21:49:37 -0800

    Add settings file.

--- blogwise/server.py
+++ blogwise/server.py
@@ -3,6 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from blogwise.views import articles, auth, index, users
 from blogwise.models import connect_to_db
+from blogwise import settings
 
 app = Flask(__name__)
 app.register_blueprint(articles)
@@ -11,10 +12,10 @@ app.register_blueprint(index)
 app.register_blueprint(users)
 
 if __name__ == '__main__':
-	app.secret_key = 'secretzzzzzz'
-	app.debug = False
-	app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-	connect_to_db(app)
+    app.secret_key = settings.SECRET_KEY
+    app.debug = False
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    connect_to_db(app)
 
-	DebugToolbarExtension(app)
-	app.run()
+    DebugToolbarExtension(app)
+    app.run()
